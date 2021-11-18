925e4ab5217f7d91f674e3dbf7159fe40e1a0045
Андрей Разумов <alexkirillof@yandex.ru>
2021-11-19 00:08:02 +0300

    Lesson1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" :isShow="true"/>
+    <!-- <button @click="increase(2,$event)">Increapse</button>
+    <button @click="reset">reset</button>
+     {{counter}} -->
+     <Calc/>
   </div>
-</template>
+ </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Calc from '@/components/Calc.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Calc
+  },
+  data () {
+    return {
+      counter: 0,
+      str: 'Hellow'
+    }
+  },
+  methods: {
+    increase (step, event) {
+      console.log(event)
+      this.counter += step
+    },
+    reset () {
+      this.counter = 0
+    },
+    input (event) {
+      console.log(event.target.value)
+    },
+    blur () {
+      console.log(blur)
+    },
+    focus () {
+      console.log(focus)
+    }
   }
 }
 </script>
@@ -24,5 +54,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button{
+  margin-left: 20px;
 }
 </style>
