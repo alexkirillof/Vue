2c2eba6d57a650473245a636db68d04fa72dcc41
Андрей Разумов <alexkirillof@yandex.ru>
2021-11-27 11:14:12 +0300

    Lesson_3

---
+++ src/components/AddPaymentForm.vue
@@ -0,0 +1,75 @@
+<template>
+  <div :class="$style.wrap">
+   <div :class="$style.addpayform">
+      <input type="text" placeholder="Date" v-model="date" :class="$style.input">
+      <input type="text" placeholder="Type" v-model="category" :class="$style.input">
+      <input type="text" placeholder="Amount" v-model="value" :class="$style.input">
+      <button @click="addPayment" :class="$style.addpaybtn">Add  +</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddPaymentForm',
+  components: {},
+  props: {},
+  data () {
+    return {
+      value: '',
+      category: '',
+      date: ''
+    }
+  },
+  methods: {
+    addPayment () {
+      const { date, category, value } = this
+      const data = { date: date || this.currentDate, category, value: +value }
+      this.$emit('add-payment', data)
+    }
+  },
+  computed: {
+    currentDate () {
+      const date = new Date()
+      return date.toLocaleDateString()
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+.wrap {
+  max-width: 900px;
+  margin: 0 auto;
+}
+.addpayform {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  clear: both;
+}
+.input {
+  padding: 5px 10px;
+  font-size: 20px;
+  color: #a2b1a3;
+  border-top:0;
+  border-left: 0;
+  border-right: 0;
+  border-bottom: 2px solid #c4cfc4;
+;
+}
+.addpaybtn{
+  align-self: flex-end;
+  color: #fff;
+  max-width: 300px;
+  font-size: 30px;
+  background-color:#2aa694;
+  padding: 5px 15px;
+  margin-bottom: 30px;
+  border: 0;
+  cursor: pointer;
+  &:hover {
+     background-color:#a0e9c8;
+  }
+}
+</style>
---
+++ src/components/PaymentsDisplay.vue
@@ -0,0 +1,69 @@
+<template>
+<div>
+  <div :class="$style.wrapper">
+      <table :class="$style.list">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Date</th>
+            <th>Category</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in items" :key="item.id">
+            <td>{{ item.id }}</td>
+            <td>{{ item.date }}</td>
+            <td>{{ item.category }}</td>
+            <td>{{ item.value }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'PaymentsDisplay',
+  props: {
+    items: {
+      type: Array,
+      required: true
+      // default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss" module>
+.wrapper {
+  width: 100%;
+  overflow-x: auto;
+}
+.list {
+  width: 100%;
+  font-size: 1.25rem;
+  border-collapse: collapse;
+  & thead {
+    color: #fff;
+    background-color: #2aa694;
+  }
+  & tbody {
+    color: #2c3e50;
+    & tr {
+      &:not(:last-child) {
+        border-bottom: 1px solid #c2c2c2;
+      }
+      &:hover {
+        background-color: lighten(#2aa694, 55%);
+      }
+    }
+  }
+  & th,
+  td {
+    padding: 0.5em;
+    text-align: center;
+  }
+}
+</style>
